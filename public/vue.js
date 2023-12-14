49d62f28e4ec2924fd2bd00f7eb60832b0a481a2
weitsung50110 <a1233z51ab10@gmail.com>
2023-12-14 17:40:55 +0800

    modify vue.js, adding all the fetches methods from script.js to vue.js

--- public/vue.js
+++ public/vue.js
@@ -2,7 +2,25 @@ const app = Vue.createApp({
     data() {
       return {
         checked: [false, false, false, false], // 包含4个boolin的数组，初始值均为 true
-        value1_checked: false,
+        value1_checked: true,
+        errorText: '',
+        userText: '',
+
+        formData: {
+            username: '',
+            email: '',
+            age: '',
+        },
+
+        formData2v: {
+            userid2v: '',
+            username2v: '',
+            email2v: '',
+            age2v: ''
+        },
+
+        deleteId: '',
+        findOneId: ''
       };
     },
     methods: {
@@ -17,7 +35,126 @@ const app = Vue.createApp({
                 document.querySelector("#find_OneOrAll_toggle").textContent = "Finding open";
             }
         },
-    }
+        async addUser() {
+            const requestOptions = {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(this.formData)
+              };
+            
+              fetch('/users', requestOptions)
+                .then(response => {
+                  if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                  }
+                  return response.json();
+                })
+                .then(data => {
+                  console.log('New user created:', data);
+                  this.errorText = JSON.stringify(data);
+                  alert('New user created:' + this.errorText);
+                })
+                .catch(error => {
+                  console.error('Error花:', error);
+                  this.errorText = 'Post Error花' + JSON.stringify(error);
+                  alert(this.errorText);
+                });
+          },
+        async updateUser() {
+            let requestOptions2 = {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(this.formData2v)
+            };
+            
+            fetch(`/users/${this.formData2v.userid2v}`, requestOptions2)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    console.log('New updateUser created:', data);
+                    this.errorText = JSON.stringify(data);
+                    alert(this.errorText);
+                })
+                .catch(error => {
+                    console.error('Error花:', error);
+                    this.errorText = 'Put你ID連輸入都沒輸入' + JSON.stringify(error);
+                    alert(this.errorText);
+                });
+        },
+        async deleteUser() {
+            const deleteUserOptions = {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            };
+        
+            fetch(`/users/${this.deleteId}`, deleteUserOptions)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    console.log('New delete created:', data);
+                    this.errorText = JSON.stringify(data);
+                    alert(this.errorText);
+                })
+                .catch(error => {
+                    console.error('Error花:', error);
+                    this.errorText = 'delete你ID連輸入都沒輸入' + JSON.stringify(error);
+                    alert(this.errorText);
+                });
+        },
+        async findOneUser() {
+            fetch(`/users/${this.findOneId}`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('New find_one created:', data);
+                let userText = '';
+                data.forEach((res_user) => {
+                    userText += `_id: ${res_user._id}; username: ${res_user.username}; email: ${res_user.email}; age: ${res_user.age} <br>`;
+                });
+                this.userText = userText;
+            })
+            .catch(error => {
+                console.error('Error花:', error);
+            });
+        },
+        async findAllUsers() {
+            fetch('/users')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('New Find created:', data);
+                let userText = '';
+                data.forEach((res_user) => {
+                    userText += `_id: ${res_user._id}; username: ${res_user.username}; email: ${res_user.email}; age: ${res_user.age} <br>`;
+                });
+                this.userText = userText;
+            })
+            .catch(error => {
+                console.error('Error花:', error);
+            });
+    }}
 })
 app.mount('.member_system');
 
